8991600bdb195b82d0d0d7e32e05e9cc419910ed
musawiraltaf <officialmail.uso2@gmail.com>
2024-12-03 21:18:48 +0500

    Create 24i-2134_A_Q3.cpp

---
+++ 24i-2134_A_Q3.cpp
@@ -0,0 +1,12 @@
+//Musawir Altaf Khan 24i-2134 Cy-A assignment 2
+#include<iostream>
+using namespace std;
+int main()
+        {
+         char a,b;
+          cout<<"Enter a Character: ";
+         cin>>a;
+         b=a+3;
+         cout<<"The result is: "<<b;
+return 0;
+        }
